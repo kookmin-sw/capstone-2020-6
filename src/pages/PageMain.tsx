ed89a6b2083f378af9fb2be42d23cad1107cbe5d
jtjisgod <jtjisgod@gmail.com>
2020-03-21 23:39:45 +0900

    Main body imaged added

--- src/pages/PageMain.tsx
+++ src/pages/PageMain.tsx
@@ -12,7 +12,15 @@ class App extends React.Component<Props> {
     console.log(this.props.navigate)
     return (
       <>
-        <div className="main_body_image" style={{backgroundImage: "url('/body.jpg')"}}/>
+        <div className="main_body_image" style={{backgroundImage: "url('/body.jpg')"}}>
+          <div>
+            <span>T-SAN, 티끌모아 태산</span>
+            <Container className="main_body_text">
+              T-SAN은 Ex dolore veniam fugiat quis adipisicing esse sunt ea enim eu consectetur ea sunt ad. Qui deserunt fugiat id aliquip esse laboris irure in adipisicing occaecat sint est. Lorem exercitation duis adipisicing veniam ullamco minim nostrud. Amet sint cillum enim dolore consectetur veniam enim id consequat commodo commodo nostrud. Aliquip eiusmod elit dolor proident qui laboris sint amet ad reprehenderit nisi.
+              Do irure sit cupidatat commodo ex dolor cupidatat et deserunt consequat non do magna aute. Cupidatat fugiat nisi consectetur consectetur tempor sint adipisicing voluptate nisi. Lorem eiusmod magna nostrud enim.
+            </Container>
+          </div>
+        </div>
       </>
     )
   }
