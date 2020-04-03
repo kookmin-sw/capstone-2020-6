369933b1a5c64bc629568a0b3e9df2b1688d2199
jtjisgod <jtjisgod@gmail.com>
2020-04-03 22:26:05 +0900

    사용자 모델에 포인트 및 신뢰도 추가

--- web-backend/tsan_server/backend/models.py
+++ web-backend/tsan_server/backend/models.py
@@ -2,10 +2,8 @@ from django.db import models
 import django.contrib.auth.models
 
 class Dataset(models.Model):
-    # PK, MongoDB에서 해당 키를 기반으로 데이터셋을 저장함
-    idx = models.AutoField(primary_key=True)
-    # 데이터셋의 이름, 최대 길이 100자
-    name = models.CharField(max_length=100)
+    idx = models.AutoField(primary_key=True) # PK, MongoDB에서 해당 키를 기반으로 데이터셋을 저장함
+    name = models.CharField(max_length=100) # 데이터셋의 이름
 
     def create(self, name):
         self.name = name
@@ -13,4 +11,6 @@ class Dataset(models.Model):
         return self
 
 class User(django.contrib.auth.models.User):
-    phone = models.CharField(max_length=30)
+    phone = models.CharField(max_length=30) # 전화번호
+    point = models.IntegerField() # 포인트
+    reliability = models.FloatField() # 신뢰도
