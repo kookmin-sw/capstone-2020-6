5769cecb91a328ca4e3600381713d60dd6d93442
jjolbo <jeongha3751@kookmin.ac.kr>
2020-04-05 01:54:18 +0900

    feat: Create mypage card

---
+++ web-frontend/src/components/MypageCard.tsx
@@ -0,0 +1,37 @@
+import React from "react";
+import {RouteComponentProps, withRouter} from "react-router-dom";
+import {Card, Button} from "semantic-ui-react";
+import './MyPageCard.css'
+
+interface Props extends RouteComponentProps<any> {
+    userType: string;
+    title: string;
+    description: string;
+    btnType: string;
+    route: string;
+}
+
+class MypageCard extends React.Component<Props> {
+    render() {
+        return (
+            <div>
+                <Card>
+                    <Card.Content className="mycard_content">
+                        <Card.Header>
+                            {this.props.title}
+                        </Card.Header>
+                        <Card.Description className="mycard_description">
+                            {this.props.description}
+                        </Card.Description>
+                    </Card.Content>
+                    <Button className="mycard_btn" href={this.props.route} color="grey">
+                        {this.props.btnType}
+                    </Button>
+                </Card>
+
+            </div>
+        );
+    }
+}
+
+export default withRouter(MypageCard);
