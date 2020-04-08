74aa7eb59ea37f5f2de1f4f0888314b239ba8daf
jjolbo <jeongha3751@kookmin.ac.kr>
2020-04-09 03:22:01 +0900

    refactor: change popup to confirm component

--- web-frontend/src/pages/PageMypage.tsx
+++ web-frontend/src/pages/PageMypage.tsx
@@ -5,7 +5,7 @@ import './PageMyPage.css'
 
 // Components
 import MypageCard from "../components/MypageCard";
-import Popup from "../components/Popup";
+import Confirm from "../components/TSANConfirm";
 // for Mobx
 import {observer, inject} from "mobx-react";
 import MyPageListStore from "../stores/MyPageListStore";
@@ -13,7 +13,6 @@ import MyPageListStore from "../stores/MyPageListStore";
 interface Props extends RouteComponentProps<any> {
     navigate?: any
     myPageListStore?: MyPageListStore;
-
 }
 
 @inject("myPageListStore")
@@ -24,16 +23,18 @@ class Mypage extends React.Component<Props> {
         super(props);
         this.props.myPageListStore!.getAvailableMyPageList();
     }
-    // Popup test
-    state = {open: false};
-    handleClose = () => this.setState({open: false});
-    handleOpen = () => this.setState({open: true});
-    // TODO: 나중에 프로젝트 삭제 등 기능 추가 구현
-    handleCorrect = () => this.setState({open: true});
+
+    // confirm test
+    state = { open: false }
+    show = () => this.setState({ open: true })
+    // TODO: 사용시, 그 상황에 맞는 함수 작성해야할 것
+    handleConfirm = () => this.setState({ open: false })
+    handleCancel = () => this.setState({ open: false })
 
     contents = ['* 한 번 취소된 프로젝트는 다시 복구할 수 없습니다.',
         '* 프로젝트 취소로 인한 수수료는 의뢰자가 부담합니다.',
     '* 정말로 취소하시겠습니까?'];
+    // end
 
     render() {
         console.log(this.props.myPageListStore!.list);
@@ -45,22 +46,21 @@ class Mypage extends React.Component<Props> {
             <div className="page">
                 <Container>
 
-                    {/*Popup test*/}
+                    {/*confirm test*/}
                     <Button
-                        content='Popup test'
-                        disabled={open}
+                        content='confirm test'
                         positive
-                        onClick={this.handleOpen}
+                        onClick={this.show}
                     />
-                    <Popup
+                    <Confirm
                         header={'경고'}
                         contents={this.contents}
                         open={open}
-                        handleClose={this.handleClose}
-                        handleCorrect={this.handleCorrect}
+                        handleConfirm={this.handleConfirm}
+                        handleCancel={this.handleCancel}
                     />
+                    {/*end*/}
 
-                    {/*real*/}
                     <Grid columns={3}>
                         {this.props.myPageListStore!.list.map((item: any, key:any) => {
                             return (
