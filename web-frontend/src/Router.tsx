c5d9a6a571502fe75e716cd88ec7bac607003a0d
jjolbo <jeongha3751@kookmin.ac.kr>
2020-04-12 02:30:27 +0900

    refactor: Add img labeling page

--- web-frontend/src/Router.tsx
+++ web-frontend/src/Router.tsx
@@ -1,9 +1,9 @@
 import React from 'react';
 import './Router.css';
 import {
-  BrowserRouter as Router,
-  Switch,
-  Route,
+    BrowserRouter as Router,
+    Switch,
+    Route,
 } from 'react-router-dom';
 
 // for Pages
@@ -13,53 +13,58 @@ import PageRegister from './pages/PageRegister';
 import PageAbout from './pages/PageAbout';
 import PageMypage from './pages/PageMypage';
 import PageMypagePW from "./pages/PageMypagePW";
+import PageImgLabeling from "./pages/PageImgLabeling";
 
 // for Components
 import Navigation from './components/Navigation';
 
 // for Mobx
-import { inject, observer } from 'mobx-react';
+import {inject, observer} from 'mobx-react';
 import MenuStore from './stores/MenuStore';
-import { useCookies } from 'react-cookie';
-import { instanceOf } from 'prop-types';
-import { withCookies, Cookies } from 'react-cookie';
+import {useCookies} from 'react-cookie';
+import {instanceOf} from 'prop-types';
+import {withCookies, Cookies} from 'react-cookie';
 
 export interface Props {
-  menuStore?: MenuStore
+    menuStore?: MenuStore
 }
 
 @inject('menuStore') @observer
 class App extends React.Component<Props> {
-  constructor(props: any) {
-    super(props);
-  }
-  render() {
-    return (
-      <Router>
-        <Navigation />
-        <Switch>
-          <Route path="/about">
-            <PageAbout />
-          </Route>
-          <Route path="/login">
-            <PageLogin />
-          </Route>
-          <Route exact path="/mypage">
-            <PageMypage/>
-          </Route>
-          <Route path="/mypage/pw">
-            <PageMypagePW/>
-          </Route>
-          <Route path="/register">
-            <PageRegister />
-          </Route>
-          <Route path="/">
-            <PageMain />
-          </Route>
-        </Switch>
-      </Router>
-    );
-  }
+    constructor(props: any) {
+        super(props);
+    }
+
+    render() {
+        return (
+            <Router>
+                <Navigation/>
+                <Switch>
+                    <Route path="/about">
+                        <PageAbout/>
+                    </Route>
+                    <Route path="/login">
+                        <PageLogin/>
+                    </Route>
+                    <Route exact path="/mypage">
+                        <PageMypage/>
+                    </Route>
+                    <Route path="/mypage/pw">
+                        <PageMypagePW/>
+                    </Route>
+                    <Route path="/register">
+                        <PageRegister/>
+                    </Route>
+                    <Route exact path="/">
+                        <PageMain/>
+                    </Route>
+                    <Route path="/labeling/img:postId">
+                        <PageImgLabeling/>
+                    </Route>
+                </Switch>
+            </Router>
+        );
+    }
 }
 
 export default App;
