60fbb152744ee284a9fb14f404661b836fc7906a
jjolbo <jeongha3751@kookmin.ac.kr>
2020-04-13 00:49:22 +0900

    refactor: Modify img selection

--- web-frontend/src/pages/PageImgLabeling.tsx
+++ web-frontend/src/pages/PageImgLabeling.tsx
@@ -5,8 +5,9 @@ import {Header, Grid, Image, Container, Button} from 'semantic-ui-react';
 // for Mobx
 import {observer, inject} from "mobx-react";
 import MyPageListStore from "../stores/MyPageListStore";
-
+import ImageLabelingSelector from '../components/ImageLabelingSelector';
 import './PageImgLabeling.css';
+
 
 interface Props extends RouteComponentProps<any> {
     navigate?: any
@@ -32,46 +33,22 @@ class Mypage extends React.Component<Props, RouteComponentProps<MatchParams>> {
             // img selection = imgSel, img capture = imgCap
             // {this.props.match.params.postId}
             <div>
-
-                <Container className="img_container">
-                    <Grid columns={3}>
-                        <Grid.Row className="img_row">
-                            <Grid.Column className="img_col">
-                                <Image className="img_size" src="https://interactive-examples.mdn.mozilla.net/media/examples/grapefruit-slice-332-332.jpg" size={"small"}/>
-                            </Grid.Column>
-                            <Grid.Column className="img_col">
-                                <Image className="img_size" src="https://miro.medium.com/max/1200/1*mk1-6aYaf_Bes1E3Imhc0A.jpeg" size={"small"}/>
-                            </Grid.Column>
-                            <Grid.Column className="img_col">
-                                <Image className="img_size" src="https://miro.medium.com/max/1200/1*mk1-6aYaf_Bes1E3Imhc0A.jpeg" size={"small"}/>
-                            </Grid.Column>
-                        </Grid.Row>
-
-                        <Grid.Row className="img_row">
-                            <Grid.Column className="img_col">
-                                <Image className="img_size" src="https://miro.medium.com/max/1200/1*mk1-6aYaf_Bes1E3Imhc0A.jpeg" size={"small"}/>
-                            </Grid.Column>
-                            <Grid.Column className="img_col">
-                                <Image className="img_size" src="https://miro.medium.com/max/1200/1*mk1-6aYaf_Bes1E3Imhc0A.jpeg" size={"small"}/>
-                            </Grid.Column>
-                            <Grid.Column className="img_col">
-                                <Image className="img_size" src="https://miro.medium.com/max/1200/1*mk1-6aYaf_Bes1E3Imhc0A.jpeg" size={"small"}/>
-                            </Grid.Column>
-                        </Grid.Row>
-
-                        <Grid.Row className="img_row">
-                            <Grid.Column className="img_col">
-                                <Image className="img_size" src="https://miro.medium.com/max/1200/1*mk1-6aYaf_Bes1E3Imhc0A.jpeg" size={"small"}/>
-                            </Grid.Column>
-                            <Grid.Column className="img_col">
-                                <Image className="img_size" src="https://miro.medium.com/max/1200/1*mk1-6aYaf_Bes1E3Imhc0A.jpeg" size={"small"}/>
-                            </Grid.Column>
-                            <Grid.Column className="img_col">
-                                <Image className="img_size" src="https://miro.medium.com/max/1200/1*mk1-6aYaf_Bes1E3Imhc0A.jpeg" size={"small"}/>
-                            </Grid.Column>
-                        </Grid.Row>
-                    </Grid>
-                </Container>
+                <ImageLabelingSelector/>
+                {/*<Container className="img_container">*/}
+                {/*    <Grid columns={3}>*/}
+                {/*        <Grid.Row className="img_row">*/}
+                {/*            <Grid.Column className="img_col">*/}
+                {/*                <Image className="img_size" src="https://interactive-examples.mdn.mozilla.net/media/examples/grapefruit-slice-332-332.jpg" size={"small"}/>*/}
+                {/*            </Grid.Column>*/}
+                {/*            <Grid.Column className="img_col">*/}
+                {/*                <Image className="img_size" src="https://miro.medium.com/max/1200/1*mk1-6aYaf_Bes1E3Imhc0A.jpeg" size={"small"}/>*/}
+                {/*            </Grid.Column>*/}
+                {/*            <Grid.Column className="img_col">*/}
+                {/*                <Image className="img_size" src="https://miro.medium.com/max/1200/1*mk1-6aYaf_Bes1E3Imhc0A.jpeg" size={"small"}/>*/}
+                {/*            </Grid.Column>*/}
+                {/*        </Grid.Row>*/}
+                {/*    </Grid>*/}
+                {/*</Container>*/}
             </div>
         );
     }
