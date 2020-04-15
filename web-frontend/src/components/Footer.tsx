c5733acb3da28f7107d0e0a41e88edd080351752
YoonYeoHwan <johnyhgmg@kookmin.ac.kr>
2020-04-15 18:22:57 +0900

    feat: Implement Footer

--- web-frontend/src/components/Footer.tsx
+++ web-frontend/src/components/Footer.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import {Container} from 'semantic-ui-react';
 import {RouteComponentProps, withRouter} from 'react-router-dom';
 
 import './Footer.css';
@@ -10,9 +11,14 @@ class Footer extends React.Component<Props> {
   render() {
     return (
       // TODO(yeohwan): Have to implement footer contents.
-      <div className='footer'>
-        Footer
-      </div>
+      <Container>
+        <div className='footer1'>
+          Copyright &copy; T-SAN of Kookmin University Capstone Project
+        </div>
+        <div className='footer2'>
+          이정하 박상일 박지희 윤여환 이다은 장태진
+        </div>
+      </Container>
     );
   }
 }
