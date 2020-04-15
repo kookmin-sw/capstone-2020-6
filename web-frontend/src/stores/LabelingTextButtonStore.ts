8e51d814f071c59315c71100ac34bf56e8564920
jtjisgod <jtjisgod@gmail.com>
2020-04-15 18:12:34 +0900

    Issue.37 해결 및 일부 컴포넌트화 진행, 그리드 문제 해결
    https://github.com/kookmin-sw/capstone-2020-6/issues/37

--- web-frontend/src/stores/LabelingTextButtonStore.ts
+++ web-frontend/src/stores/LabelingTextButtonStore.ts
@@ -2,24 +2,37 @@ import {action, observable} from 'mobx';
 
 export default class LabelingTextButtonStore {
     @observable buttonList: any = [];
+    @observable activeButton: any = -1;
+
     constructor() {
       this.buttonList = [];
+      this.activeButton = -1;
     }
-    @action getButtons() {
+
+    @action setActiveButton = (id: any) => {
+      this.activeButton = id
+    }
+
+    @action getButtons = () => {
       this.buttonList = [
         {
+          id: 1,
           category: '일반 광고',
         },
         {
+          id: 2,
           category: '선거 홍보',
         },
         {
+          id: 3,
           category: '사기 범죄',
         },
         {
+          id: 4,
           category: '개인 문자',
         },
         {
+          id: 5,
           category: '택배 안내',
         },
       ];
