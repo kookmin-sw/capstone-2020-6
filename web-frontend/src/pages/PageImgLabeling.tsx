af5c5bb987b57c834f7a51d531e6f888e6ea6f83
jjolbo <jeongha3751@kookmin.ac.kr>
2020-04-17 22:33:33 +0900

    refactor: Add image selection labeling

--- web-frontend/src/pages/PageImgLabeling.tsx
+++ web-frontend/src/pages/PageImgLabeling.tsx
@@ -4,52 +4,60 @@ import {Header, Grid, Image, Container, Button} from 'semantic-ui-react';
 
 // for Mobx
 import {observer, inject} from "mobx-react";
-import MyPageListStore from "../stores/MyPageListStore";
-import ImageLabelingSelector from '../components/ImageLabelingSelector';
-import './PageImgLabeling.css';
 
+import ImagePicker from "../components/image_picker";
 
 interface Props extends RouteComponentProps<any> {
     navigate?: any
-    myPageListStore?: MyPageListStore;
     type?: string;
+}
+
+interface State {
+    image: any;
+    images: any[];
 }
 
 interface MatchParams {
     postId: string;
 }
 
-@inject("myPageListStore")
-@observer
-class Mypage extends React.Component<Props, RouteComponentProps<MatchParams>> {
+const img1 = "https://www.topstarnews.net/news/photo/202001/721626_434936_4849.jpg";
+
+const imageList = [img1, img1, img1, img1, img1, img1, img1, img1, img1];
+
+class Mypage extends React.Component<Props, State, RouteComponentProps<MatchParams>> {
 
     constructor(props: any) {
         super(props);
-        this.props.myPageListStore!.getAvailableMyPageList();
+        this.state = {
+            image: null,
+            images: []
+        }
+    }
+
+    onPickImage = (image: any) => {
+        this.setState({image})
+    }
+    onPickImages = (images: any) => {
+        this.setState({images});
     }
 
     render() {
         return (
-            // img selection = imgSel, img capture = imgCap
-            // {this.props.match.params.postId}
-            <div>
-                <ImageLabelingSelector/>
-                {/*<Container className="img_container">*/}
-                {/*    <Grid columns={3}>*/}
-                {/*        <Grid.Row className="img_row">*/}
-                {/*            <Grid.Column className="img_col">*/}
-                {/*                <Image className="img_size" src="https://interactive-examples.mdn.mozilla.net/media/examples/grapefruit-slice-332-332.jpg" size={"small"}/>*/}
-                {/*            </Grid.Column>*/}
-                {/*            <Grid.Column className="img_col">*/}
-                {/*                <Image className="img_size" src="https://miro.medium.com/max/1200/1*mk1-6aYaf_Bes1E3Imhc0A.jpeg" size={"small"}/>*/}
-                {/*            </Grid.Column>*/}
-                {/*            <Grid.Column className="img_col">*/}
-                {/*                <Image className="img_size" src="https://miro.medium.com/max/1200/1*mk1-6aYaf_Bes1E3Imhc0A.jpeg" size={"small"}/>*/}
-                {/*            </Grid.Column>*/}
-                {/*        </Grid.Row>*/}
-                {/*    </Grid>*/}
-                {/*</Container>*/}
-            </div>
+            <Container style={{marginTop: 50}}>
+                <Header as='h2'>라벨링 주제명</Header>
+                <Header as='h4'>#1. 남자를 선택하시오.</Header>
+                <Grid>
+                    <Grid.Row columns={3}>
+                        <ImagePicker
+                            images={imageList.map((image, i) => ({src: image, value: i}))}
+                            onPick={this.onPickImages.bind(this)}
+                            multiple
+                        />
+                    </Grid.Row>
+                </Grid>
+                {console.log(this.state.images)}
+            </Container>
         );
     }
 }
