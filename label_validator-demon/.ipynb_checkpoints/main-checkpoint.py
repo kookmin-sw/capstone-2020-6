be0786fb9e61a29b42a7408e9aebab9dfcf8653b
psi1104 <sangil9117@gmail.com>
2020-04-17 10:24:48 +0000

    find_modeo_label 추가, temp_labeling 수정

--- label_validator-demon/.ipynb_checkpoints/main-checkpoint.py
+++ label_validator-demon/.ipynb_checkpoints/main-checkpoint.py
@@ -2,6 +2,7 @@ import model
 import clustering
 
 import pandas as pd
+import operator
 
 
 def make_dataframe() :
@@ -10,7 +11,21 @@ def make_dataframe() :
     
     return df
 
+def find_mode_label(labels):
+    label_count = {}
+    labels_set = set(labels)
+    
+    for label in labels_set:
+        label_count[label] = labels.count(label)
+        
+    label_count = sorted(label_count.items(), key=operator.itemgetter(1), reverse=True)
+    
+    return label_count[0][0]
+
+
 def temp_labeling(df):
+    df_temp_labeling = pd.DataFrame({'file_index' : [], 'path' : [], 'label_temp' : []})
+    
     file_indices = set(df.file_index.tolist())
     
     for i in file_indices:
@@ -18,11 +33,16 @@ def temp_labeling(df):
         
         credibilities = temp_df.credibility.tolist()
         credibilities.sort()
-         
+        
         if len(credibilities) > 2:
             labels = temp_df[temp_df['credibility'] >= credibilities[-3]].label.tolist()
+            mode_label = find_mode_label(labels)
+        
+        
+        data = pd.Series([i, temp_df['path'].iloc[0], mode_label], index = ['file_index', 'path', 'label_temp'])  
+        df_temp_labeling = df_temp_labeling.append(data, ignore_index = True)
             
-    return new_df
+    return df_temp_labeling
 
 
 def label_validate(df):
