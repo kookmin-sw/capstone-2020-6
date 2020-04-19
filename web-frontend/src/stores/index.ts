fb5093d44413d600ef8fefbbf93f189528a53fcb
YoonYeoHwan <johnyhgmg@kookmin.ac.kr>
2020-04-19 19:16:18 +0900

    fix: Fix typo

--- web-frontend/src/stores/index.ts
+++ web-frontend/src/stores/index.ts
@@ -15,5 +15,5 @@ export default {
   labelingTextButtonStore: new LabelingTextButtonStore(),
   labelingImgStore: new LabelingImgStore(),
   labelingPageStore: new LabelingPageStore(),
-  mypageListStore: new MyPageListStore(),
+  myPageListStore: new MyPageListStore(),
 };
