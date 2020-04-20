0fd375f2a69b29a7f999fcfa215a6b504ed33672
jjolbo <jeongha3751@kookmin.ac.kr>
2020-04-21 05:36:59 +0900

    refactor: Add id

--- web-frontend/src/stores/ProjectListStore.ts
+++ web-frontend/src/stores/ProjectListStore.ts
@@ -14,6 +14,7 @@ export default class ProjectListStore {
   @action getAvailableProject = () => {
     this.list = [
       {
+        id: '1',
         thumbnail:
           "https://image.dongascience.com/Photo/2018/01/15159739972169[1].jpg",
         title: "강아지 비문 데이터 라벨링",
@@ -28,6 +29,7 @@ export default class ProjectListStore {
         progress_rate: "65"
       },
       {
+        id: '2',
         thumbnail:
           "https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcSEejDh2cKhbSC28LHNkZ2-PEIiW3Jw06xa-n_Cq17NlvIurwVO",
         title: "구름 데이터 라벨링",
@@ -42,6 +44,7 @@ export default class ProjectListStore {
         progress_rate: "7"
       },
       {
+        id: '3',
         thumbnail:
           "https://imagescdn.gettyimagesbank.com/500/16/933/891/0/504644388.jpg",
         title: "암세포 판단을 위한 데이터 라벨링",
@@ -56,6 +59,7 @@ export default class ProjectListStore {
         progress_rate: "0"
       },
       {
+        id: '4',
         thumbnail:
           "https://pds.joins.com/news/component/htmlphoto_mmdata/201308/06/htm_201308061479a010a011.jpg",
         title: "고흐 작품 판단",
