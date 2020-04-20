cf51ce5b87113fd8280075ac5e2cdd0be742ac4a
psi1104 <sangil9117@gmail.com>
2020-04-20 16:43:45 +0000

    cnn_model 수정

--- src/label_validator-demon/cnn_model.py
+++ src/label_validator-demon/cnn_model.py
@@ -1,30 +1,34 @@
 # -*- coding: utf-8 -*-
 import numpy as np
-from keras.preprocessing import image
-from keras.applications.resnet50 import ResNet50
-from keras.applications.resnet50 import preprocess_input, decode_predictions
+from keras.preprocessing.image import ImageDataGenerator
+from keras.applications.densenet import DenseNet201
+from keras.applications.densenet import preprocess_input, decode_predictions
 
-def model():
+def conv_base():
     
-    model = ResNet50(weights='imagenet', include_top=False)
+    conv_base = DenseNet201(include_top = False, weights='imagenet', input_shape=(224,224,3))
     
-    return model
+    return conv_base
 
-def feature_extract(model, path):
+def extract_features(df, sample_count, num_classes):
     
-    img_feature_list = []
-    
-    for img_path in path:
-
-        img = image.load_img(img_path, target_size=(224,224))
-        img_data = image.img_to_array(img)
-
-        img_data = np.expand_dims(img_data, axis=0)
-        img_data = preprocess_input(img_data)
-
-        #모델로 특징 추출
-        img_feature = model.predict(img_data)
-        img_feature_np = np.array(img_feature)
-        img_feature_list.append(img_feature_np.flatten())
-    
-    return img_feature_list
+    datagen = ImageDataGenerator(rescale=1./255)
+    features = np.zeros(shape=(sample_count, 7, 7, 1920))  # Must be equal to the output of the convolutional base
+    labels = np.zeros(shape=(sample_count, num_classes))
+    # Preprocess data
+    generator = datagen.flow_from_dataframe(df,
+                                            x_col='path',
+                                            y_col='label',
+                                            target_size=input_shape[:2],
+                                            batch_size=batch_size  
+                                           )
+    # Pass data through convolutional base
+    i = 0
+    for inputs_batch, labels_batch in generator:
+        features_batch = conv_base.predict(inputs_batch)
+        features[i * batch_size: (i + 1) * batch_size] = features_batch
+        labels[i * batch_size: (i + 1) * batch_size] = labels_batch
+        i += 1
+        if i * batch_size >= sample_count:
+            break
+    return features, labels
