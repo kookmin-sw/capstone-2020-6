c2153ecf6d4257b4cb26ce90e8050dfc1bc7b647
parkjihee100 <jihi1013@kookmin.ac.kr>
2020-04-21 20:00:23 +0900

    StandardPoint.sol 나머지 함수

--- src/blockchain/contracts/StandardPoint.sol
+++ src/blockchain/contracts/StandardPoint.sol
@@ -24,17 +24,28 @@ contract StandardPoint is Point {
     }
 
     function supply(uint256 _value) returns (bool success) {
+        require(_value > 0);
+
+        balances[fundsUser] += _value;
+        totalSupply += _value;
+        return true;
     }
 
 
     function balanceOf(string user) constant returns (uint256 balance) {
+        return balances[user];
     }
 
     function approve(string _spender, uint256 _value) returns (bool success) {
+        allowed["owner"][_spender] = _value;
+        Approval("owner", _spender, _value);
+        return true;
     }
 
     function allowance(string _owner, string _spender) constant returns (uint256 remaining) {
+      return allowed[_owner][_spender];
     }
+
 
     mapping (string => uint256) balances;
     mapping (string => mapping (string => uint256)) allowed;
