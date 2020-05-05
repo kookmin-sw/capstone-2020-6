f943ef054c6ac796d0b2f1fa4331f1ae2675a8fb
jjolbo <jeongha3751@kookmin.ac.kr>
2020-05-05 17:41:22 +0900

    refactor: Change imgselstore

--- src/web-frontend/src/pages/PageLabelingImgSelect.tsx
+++ src/web-frontend/src/pages/PageLabelingImgSelect.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import {RouteComponentProps, withRouter} from 'react-router-dom';
 import {Header, Grid, Container} from 'semantic-ui-react';
 import ImagePicker from "../components/image_picker";
+import LabelingNextBtn from "../components/LabelingNextBtn";
 
 // for Mobx
 import {observer, inject} from "mobx-react";
@@ -48,18 +49,34 @@ class PageLabelingImgSel extends React.Component<Props, State, RouteComponentPro
         this.props.labelingImgStore!.setSelectList(this.state.images)
     }
 
+    // 라벨링 다음 버튼 onclick 함수 부분(이런 식으로 만들어서 넣으면 될 듯)
+    handleLink = (e: any) => {
+        // TODO: 선택한 사진 API로 보내기
+        var num = parseInt(this.props.match.params.dataId)+1;
+        this.props.history.push(`/labeling/${this.props.match.params.postId}/imgsel/${num}`);
+    }
+
     render() {
         return (
             <Container style={{marginTop: 50}}>
                 <Header as='h2'>강아지 이미지 데이터를 통한 이미지 학습 모델 구현</Header>
                 <Header as='h4'>#1. 강아지를 선택하시오.</Header>
                 <Grid>
-                    <Grid.Row columns={3}>
-                        <ImagePicker
-                            images={this.props.labelingImgStore!.imgList.map((image, i) => ({src: image, value: i}))}
-                            onPick={this.onPickImages.bind(this)}
-                            multiple
-                        />
+                    <Grid.Row columns={2}>
+                        <Grid.Column>
+                            <ImagePicker
+                                images={this.props.labelingImgStore!.imgList[0].data.map((image:string, i:number) => ({
+                                    src: image,
+                                    value: i
+                                }))}
+                                onPick={this.onPickImages.bind(this)}
+                                multiple
+                            />
+                        </Grid.Column>
+                        <Grid.Column>
+                            {/*라벨링 다음 버튼 부분*/}
+                            <LabelingNextBtn handleLink={this.handleLink}/>
+                        </Grid.Column>
                     </Grid.Row>
                 </Grid>
             </Container>
