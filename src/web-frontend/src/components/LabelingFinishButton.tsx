dd916b29ddc68206b5f8fcd213d481d0e85809be
YoonYeoHwan <johnyhgmg@kookmin.ac.kr>
2020-05-08 15:59:47 +0900

    feat: Change labeling finish button redirect

--- src/web-frontend/src/components/LabelingFinishButton.tsx
+++ src/web-frontend/src/components/LabelingFinishButton.tsx
@@ -1,36 +1,14 @@
 import React from 'react';
 import {Button} from 'semantic-ui-react';
-import {withRouter, RouteComponentProps, Link} from 'react-router-dom';
-import {History, LocationState} from 'history';
+import {Link} from 'react-router-dom';
 
 import './LabelingFinishButton.css';
 
-interface Props {
-  history: History<LocationState>;
-  match: any;
-}
-
-interface State {
-  postId: string;
-}
-
-class LabelingFinishButton extends React.Component<Props, State, RouteComponentProps> {
-  constructor(props: any) {
-    super(props);
-    this.state = {
-      postId: '',
-    };
-  }
-
-  UNSAFE_componentWillMount(): void {
-    const parserArray = this.props.match.url.split('/');
-    this.setState({postId: parserArray[2]});
-  }
-
+class LabelingFinishButton extends React.Component {
   render() {
     return (
       <div>
-        <Link to={`/labeling/${this.state.postId}/finish`}>
+        <Link to={`/mypage/projects`}>
           <Button
             className='finishBtn'
             color={'red'}>
@@ -42,5 +20,4 @@ class LabelingFinishButton extends React.Component<Props, State, RouteComponentP
   }
 }
 
-// @ts-ignore
-export default withRouter(LabelingFinishButton);
+export default LabelingFinishButton;
