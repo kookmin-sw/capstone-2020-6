242bda36023ad6d0b42f09c742d95761aec37801
jtjisgod <jtjisgod@gmail.com>
2020-05-08 22:14:58 +0900

    Mysql 연동

--- src/web-backend/tsan_server/test.py
+++ src/web-backend/tsan_server/test.py
@@ -1,22 +1,24 @@
 import random
 from mongodb import db
 
-cnt = db.text_dataset.estimated_document_count()
 dataset = []
+print(">> 1")
 rows = db.text_dataset.aggregate(
-    [
-        { "$sample": { "size": 1000 } },
-        # {
-        #     "$project": {
-        #         "_id": 1
-        #     }
-        # }
-    ]
+    [{
+        "$sample": {
+            "size": 1000
+        }
+    }]
 )
+print(">> 2")
 rows = [x['_id'] for x in rows]
+print(">> 3")
 print(len(rows))
-dataset.extend(rows)
-print(dataset)
+print(">> 4")
+
+# dataset.extend(rows)
+
+# print(dataset)
 
 
 # rows = db.text_dataset.find(skip=random.rand)
