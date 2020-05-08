7040d602bb0b9b3a8cd3c2100a611b7d4a49931b
YoonYeoHwan <johnyhgmg@kookmin.ac.kr>
2020-05-08 16:48:50 +0900

    feat: Add Confirm Component in labeling finhish

--- src/web-frontend/src/components/LabelingFinishButton.tsx
+++ src/web-frontend/src/components/LabelingFinishButton.tsx
@@ -1,23 +1,56 @@
 import React from 'react';
 import {Button} from 'semantic-ui-react';
-import {Link} from 'react-router-dom';
+import {History, LocationState} from 'history';
+import {RouteComponentProps, withRouter} from 'react-router';
+
+// Components
+import Confirm from './TSANConfirm';
 
 import './LabelingFinishButton.css';
 
-class LabelingFinishButton extends React.Component {
+interface Props {
+  history: History<LocationState>;
+}
+
+interface State {
+  open: boolean;
+}
+
+class LabelingFinishButton extends React.Component<Props, State, RouteComponentProps> {
+  constructor(props: any) {
+    super(props);
+    this.state = {
+      open: false,
+    };
+  }
+
+  show = () => this.setState({open: true});
+  handleConfirm = () => {
+    this.setState({open: false});
+    this.props.history.push('/mypage/projects');
+  };
+  handleCancel = () => this.setState({open: false});
   render() {
+    const {open} = this.state;
     return (
       <div>
-        <Link to={`/mypage/projects`}>
-          <Button
-            className='finishBtn'
-            color={'red'}>
-            종료
-          </Button>
-        </Link>
+        <Button
+          className='finishBtn'
+          color={'red'}
+          onClick={this.show}>
+          종료
+        </Button>
+        <Confirm
+          header={'알림'}
+          contents={['프로젝트를 완료하시겠습니까?']}
+          open={open}
+          handleConfirm={this.handleConfirm}
+          handleCancel={this.handleCancel}
+        />
       </div>
     );
   }
 }
 
-export default LabelingFinishButton;
+// @ts-ignore
+export default withRouter(LabelingFinishButton);
