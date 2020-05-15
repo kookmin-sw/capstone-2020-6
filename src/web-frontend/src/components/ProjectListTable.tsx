9b1e9e994640e0197fbe81c96c70aa31ee55c853
YoonYeoHwan <johnyhgmg@kookmin.ac.kr>
2020-05-15 13:38:52 +0900

    feat: Project List Component V1

---
+++ src/web-frontend/src/components/ProjectListTable.tsx
@@ -0,0 +1,38 @@
+import React from 'react';
+import {RouteComponentProps, withRouter} from 'react-router-dom';
+import {Table} from 'semantic-ui-react';
+
+interface Props extends RouteComponentProps<any> {
+  header: any
+  body: any
+}
+
+class ProjectListTable extends React.Component<Props> {
+  constructor(props: any) {
+    super(props);
+  }
+  render() {
+    return (
+      <div>
+        <Table celled>
+          <Table.Header>
+            <Table.Row>
+              {this.props.header.map((item: any) => {
+                return (
+                  <Table.HeaderCell key={item.id}>
+                    {item.headerItem}
+                  </Table.HeaderCell>
+                );
+              })}
+            </Table.Row>
+          </Table.Header>
+          <Table.Body>
+            {this.props.body}
+          </Table.Body>
+        </Table>
+      </div>
+    );
+  }
+}
+
+export default withRouter(ProjectListTable);
