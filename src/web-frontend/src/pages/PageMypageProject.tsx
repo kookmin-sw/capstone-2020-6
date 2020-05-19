739efca7a98e0bb3f89d0903ff6e6cb480e63017
jjolbo <jeongha3751@kookmin.ac.kr>
2020-05-20 01:45:38 +0900

    refactor: Add start, end function

--- src/web-frontend/src/pages/PageMypageProject.tsx
+++ src/web-frontend/src/pages/PageMypageProject.tsx
@@ -11,6 +11,10 @@ import ProjectListTable from "../components/ProjectListTable";
 interface Props {
     myPageProjectStore?: MyPageProjectStore;
 }
+interface State {
+    open: boolean;
+    type: string;
+}
 
 @inject('myPageProjectStore')
 @observer
@@ -19,11 +23,16 @@ interface Props {
 //
 // }
 
-class PageMypageProject extends React.Component<Props> {
+class PageMypageProject extends React.Component<Props,State> {
     constructor(props: any) {
         super(props);
         // 답변 가능한 프로젝트 목록 요청
         this.props.myPageProjectStore!.getAvailableProject();
+        this.state = {
+            open: false,
+            type: '',
+        }
+        this.show = this.show.bind(this);
     }
     header = [
         {id: 1, headerItem: '#'},
@@ -31,16 +40,23 @@ class PageMypageProject extends React.Component<Props> {
         {id: 3, headerItem: '레이블링 유형'},
         {id: 4, headerItem: '시작일'},
         {id: 5, headerItem: '마감일'},
-        {id: 6, headerItem: '수정'},
-        {id: 7, headerItem: '삭제'},
+        {id: 6, headerItem: '시작'},
+        {id: 7, headerItem: '종료'},
         {id: 8, headerItem: '비고'},
     ];
 
-    state = {open: false}
-    show = () => this.setState({open: true})
+    show = (_type:string) => {
+        this.setState({open: true});
+        this.setState({type: _type});
+    }
 
-    handleConfirm = () => this.setState({open: false})
-    handleCancel = () => this.setState({open: false})
+    handleConfirm = () => {
+        // TODO: API 연동
+        this.setState({open: false})
+    }
+    handleCancel = () => {
+        this.setState({open: false})
+    }
 
     render() {
         const {open} = this.state;
@@ -57,13 +73,13 @@ class PageMypageProject extends React.Component<Props> {
                                 <Table.Cell>2020.02.03</Table.Cell>
                                 <Table.Cell>2020.03.03</Table.Cell>
                                 <Table.Cell>
-                                    <Link to={'/updateRegister'}>
-                                        <Icon name="pencil alternate"/>
-                                    </Link>
+                                    <div onClick={()=>this.show('start')}>
+                                        <Icon name="sign-in alternate"/>
+                                    </div>
                                 </Table.Cell>
                                 <Table.Cell>
-                                    <div onClick={this.show}>
-                                        <Icon name="trash alternate"/>
+                                    <div onClick={()=>this.show('end')}>
+                                        <Icon name="sign-out alternate"/>
                                     </div>
                                 </Table.Cell>
                                 <Table.Cell>{item.status}</Table.Cell>
@@ -74,7 +90,7 @@ class PageMypageProject extends React.Component<Props> {
 
                 <Confirm
                     header={'안내'}
-                    contents={['프로젝트를 정말로 삭제하시겠습니까?']}
+                    contents={this.state.type==='start'?['지금 시작하시겠습니까?']:['지금 종료하시겠습니까?']}
                     open={open}
                     handleConfirm={this.handleConfirm}
                     handleCancel={this.handleCancel}
