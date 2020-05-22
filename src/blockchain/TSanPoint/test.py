eb1d735e6a00b007d880fe4d2e4ef8a6a20f290e
parkjihee100 <jihi1013@kookmin.ac.kr>
2020-05-22 13:54:35 +0900

    블록체인 파일

---
+++ src/blockchain/TSanPoint/test.py
@@ -0,0 +1,7 @@
+import TSanPoint
+
+print(TSanPoint.totalSupply())
+print(TSanPoint.balanceOf('owner'))
+print(TSanPoint.balanceOf('kookmin'))
+print(TSanPoint.transfer('owner','kookmin',100))
+print(TSanPoint.approve('owwner',1000))
