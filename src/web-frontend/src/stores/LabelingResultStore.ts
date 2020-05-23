1243fb345080eaab5e562be9265c7c2b7956275c
jjolbo <jeongha3751@kookmin.ac.kr>
2020-05-23 18:47:23 +0900

    refactor: Add labeling result store

--- src/web-frontend/src/stores/LabelingResultStore.ts
+++ src/web-frontend/src/stores/LabelingResultStore.ts
@@ -1,28 +1,41 @@
 import {action, observable} from 'mobx';
 
-export default class LabelingResult {
+export default class LabelingResultStore {
     // @observable labelingSubject: any = '';
-    @observable all_user: any = [];
-    @observable project_user: any = [];
+    @observable levelData: any = [];
+    @observable labelingResult: any = [];
 
     constructor() {
-        this.all_user = [];
-        this.project_user = [];
+        this.levelData = [];
+        this.labelingResult = [];
     }
 
-    @action getAllUser = () => {
-        this.all_user = [
+    @action getLevelData = () => {
+        this.levelData = [
             {
-                '동네 뒷산': 4000
+                name: '동네 뒷산', All_user: 4000, Project_user: 2400,
+            },
+            {
+                name: '지리산', All_user: 3000, Project_user: 1398,
+            },
+            {
+                name: '한라산', All_user: 9800, Project_user: 2000,
+            },
+            {
+                name: '에베레스트', All_user: 2780, Project_user: 3908,
+            },
+            {
+                name: '티산', All_user: 4800, Project_user: 1980,
             }
         ]
     }
-    @action getProjectUser = () => {
+
+    @action getLabelingResult = () => {
+        this.labelingResult = [
+            { name: 'Dog\'s nose', value: 400 }, { name: 'Muffine', value: 300 },
+            { name: 'None', value: 30 },
+        ];
 
     }
-
-    // @action getLabelingSubject = () => {
-    //     this.labelingSubject = '광고 문자 필터링을 위한 Labeling';
-    // }
 
 }
