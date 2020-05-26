12a3592d9af49c231eece70c1d6fefe21cfcc6a8
jjolbo <jeongha3751@kookmin.ac.kr>
2020-05-26 22:46:00 +0900

    refactor: Chage header message

--- src/web-frontend/src/components/Header.tsx
+++ src/web-frontend/src/components/Header.tsx
@@ -19,8 +19,8 @@ class Header extends React.Component<Props> {
           <Container className="main_body_text">
             <br/>
             <div>티끌 모아 태산 T-SAN에서 데이터 라벨링을 통한 보상을 얻어보세요.</div>
-            저희 T-SAN 플랫폼은 일반적인 데이터 레이블링 의뢰뿐만 아니라 자체적으로 데이터셋을 확보하여<br/>
-            레이블링을 진행해서 레이블링된 데이터를 공공 데이터로 공개하여 인공지능 사업 발전에 기여할 수 있습니다.
+            저희 T-SAN 플랫폼은 자체적으로 데이터셋을 확보하고<br/>
+            레이블링을 진행하여 인공지능 사업 발전에 기여할 수 있습니다.
           </Container>
         </div>
       </div>
