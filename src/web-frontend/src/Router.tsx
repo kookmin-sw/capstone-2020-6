e5b3ff28e3168f9d2c62e04605f08a6403dfa236
YoonYeoHwan <johnyhgmg@kookmin.ac.kr>
2020-05-26 20:05:34 +0900

    feat: Change button clicked status when move to next labeling

--- src/web-frontend/src/Router.tsx
+++ src/web-frontend/src/Router.tsx
@@ -78,11 +78,11 @@ class App extends React.Component<Props> {
           <Route path='/register'>
             <PageRegister/>
           </Route>
-          <Route path="/labeling/:postId/1/:dataId">
-            <PageLabelingTextSelect/>
-          </Route>
           <Route path='/labeling/:postId/0/:dataId'>
             <PageLabelingTextWrite/>
+          </Route>
+          <Route path="/labeling/:postId/1/:dataId">
+            <PageLabelingTextSelect/>
           </Route>
           <Route path="/labeling/:postId/2/:dataId">
             <PageLabelingImgSelect/>
