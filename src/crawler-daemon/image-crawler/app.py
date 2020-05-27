57c40cbdd73426ecbab83ca431a9cb4a7d5ad5e0
jtjisgod <jtjisgod@gmail.com>
2020-05-27 18:17:59 +0900

    Project 복구

--- src/crawler-daemon/image-crawler/app.py
+++ src/crawler-daemon/image-crawler/app.py
@@ -1,12 +1,7 @@
-from icrawler.builtin import GoogleImageCrawler
+from google_images_download import google_images_download   #importing the library
 
-query = "침실"
-output = "./bedroom"
+response = google_images_download.googleimagesdownload()   #class instantiation
 
-google_crawler = GoogleImageCrawler(
-    feeder_threads=1,
-    parser_threads=1,
-    downloader_threads=4,
-    storage={'root_dir': output})
-
-google_crawler.crawl(keyword=query, offset=0, max_num=1000, min_size=(200,200), max_size=None, file_idx_offset=0)
+arguments = {"keywords":"bedroom", "limit":20, "print_urls":True}
+paths = response.download(arguments)
+print(paths)
