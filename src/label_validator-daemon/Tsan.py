8c21726578e667774e54a6d9ac6c2c901434a703
jtjisgod <jtjisgod@gmail.com>
2020-06-06 04:38:14 +0900

    Image file down.

--- src/label_validator-daemon/Tsan.py
+++ src/label_validator-daemon/Tsan.py
@@ -1,5 +1,7 @@
 import requests
 import json
+import os
+from bson import ObjectId
 from mongodb import db
 from graphqlclient import GraphQLClient
 
@@ -57,11 +59,29 @@ class Tsan:
     
     def download(self, request):
 
-        # 이미지인 경우
-        if request['category']['type'] == "image":
-            
+        folder = "./tmp/%s/"%(request['idx'])
+
+        try:
+            os.makedirs(folder)
+        except:
             pass
 
+        assigned = db.assigned_dataset.find_one({"request": int(request['idx'])})
+
+        # 이미지인 경우
+        if request['category']['type'] == "image":
+            i = 0
+            files = []
+            for image in assigned['dataset']:
+                i += 1
+                img = db.image_dataset.find_one({"_id": ObjectId(image)})
+                filename = os.path.join(folder, img['filename'])
+                f = open(filename, "wb")
+                print("[%d/%d] Download %s"%(i, len(assigned['dataset']), img['filename']))
+                f.write(img['data'])
+                f.close()
+                files.append(filename)
+            return files
         # 텍스트인 경우
         else:
             pass
--- src/label_validator-daemon/daemon.py
+++ src/label_validator-daemon/daemon.py
@@ -1,34 +1,48 @@
 from Tsan import Tsan
 
+files = []
+
 def text_select(tsan, request):
+    global files
     pass
 
 def image_choice(tsan, request):
+    global files
     pass
 
 def image_capture_validator(tsan, request):
+    global files
     pass
 
 def image_select(tsan, request):
+    global files
     pass
 
 def main():
+    global files
+
     tsan = Tsan()
     tsan.login(username="robot", password="robot")
     if not tsan.token:
         print("Login Fail")
         return
 
-    tsan.get_end_requests()    
+    tsan.get_end_requests()
     for request in tsan.requests:
-        if request['category']['idx'] == 1: # 텍스트 객관식
-            text_select(tsan, request)
-        elif request['category']['idx'] == 2: # 이미지 선택형
-            image_choice(tsan, request)
-        elif request['category']['idx'] == 3: # 이미지 영역지정
-            image_capture_validator(tsan, request)
-        elif request['category']['idx'] == 4: # 이미지 객관식
-            image_select(tsan, request)
+        try:
+            files = tsan.download(request)
+
+            if request['category']['idx'] == 1: # 텍스트 객관식
+                text_select(tsan, request)
+            elif request['category']['idx'] == 2: # 이미지 선택형
+                image_choice(tsan, request)
+            elif request['category']['idx'] == 3: # 이미지 영역지정
+                image_capture_validator(tsan, request)
+            elif request['category']['idx'] == 4: # 이미지 객관식
+                image_select(tsan, request)
+        except Exception as e:
+            print("Error: ", e)
+            pass
 
 if __name__ == "__main__":
     main()
