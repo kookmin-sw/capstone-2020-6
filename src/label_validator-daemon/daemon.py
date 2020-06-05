238b1a627709d7ff81980ba434769e27c131a252
jtjisgod <jtjisgod@gmail.com>
2020-06-06 04:09:15 +0900

    라벨 별 함수로 분기

--- src/label_validator-daemon/daemon.py
+++ src/label_validator-daemon/daemon.py
@@ -1,5 +1,17 @@
 from Tsan import Tsan
 
+def text_select(tsan, request):
+    pass
+
+def image_choice(tsan, request):
+    pass
+
+def image_capture_validator(tsan, request):
+    pass
+
+def image_select(tsan, request):
+    pass
+
 def main():
     tsan = Tsan()
     tsan.login(username="robot", password="robot")
@@ -10,13 +22,13 @@ def main():
     tsan.get_end_requests()    
     for request in tsan.requests:
         if request['category']['idx'] == 1: # 텍스트 객관식
-            pass
+            text_select(tsan, request)
         elif request['category']['idx'] == 2: # 이미지 선택형
-            pass
+            image_choice(tsan, request)
         elif request['category']['idx'] == 3: # 이미지 영역지정
-            pass
+            image_capture_validator(tsan, request)
         elif request['category']['idx'] == 4: # 이미지 객관식
-            pass
+            image_select(tsan, request)
 
 if __name__ == "__main__":
     main()
