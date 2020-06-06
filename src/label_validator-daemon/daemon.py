3f7913b49987615e9ff149689cdee5a9f33942aa
jtjisgod <jtjisgod@gmail.com>
2020-06-06 22:26:20 +0900

    Daemon

--- src/label_validator-daemon/daemon.py
+++ src/label_validator-daemon/daemon.py
@@ -1,7 +1,26 @@
 from Tsan import Tsan
+import json
+from numpy import dot
+from numpy.linalg import norm
+import numpy as np
+
+def cos_sim(A, B):
+    return dot(A, B)/(norm(A)*norm(B))
+
+def mean(dddd):
+    removed = []
+    lst = []
+    mean_x = np.mean(dddd)
+    for idx, x in enumerate(dddd):
+        if abs((mean_x - x)/mean_x) >= 0.3:
+            removed.append(idx)
+        else:
+            lst.append(x)
+    return np.mean(dddd), removed
 
 tsan = Tsan()
 files = []
+users = []
 
 def text_select(request):
     pass
@@ -10,28 +29,59 @@ def image_choice(request):
     pass
 
 def image_capture_validator(request):
-    print("HI")
-    users = tsan.getLabelPerUser(request)
-    for username, values in users.items():
-        print(username)
-        print(values['dataset'])
+    global users
+    for data_key, filename in files.items():
+        dddd = {
+            "users": [],
+            "x": [],
+            "y": [],
+            "width": [],
+            "height": []
+        }
+        for username, data in users.items():
+            if data['dataset'][data_key] == None:
+                continue
+            res = json.loads(data['dataset'][data_key])
+            dddd['users'].append(username)
+            dddd['x'].append(res.get("x"))
+            dddd['y'].append(res.get("y"))
+            dddd['width'].append(res.get("width"))
+            dddd['height'].append(res.get("height"))
+        
+        removed = []
+        x, r = mean(dddd['x'])
+        removed += r
+        y, r = mean(dddd['y'])
+        removed += r
+        width, r = mean(dddd['width'])
+        removed += r
+        height, r = mean(dddd['height'])
+        removed += r
+
+        for i, in enumerate(users):
+            users[dddd['users'][i]]['reliability'] *= 0.99 if i in removed else 1.01
+        
+        print([x, y, width, height])        
 
 def image_select(request):
     pass
 
 def main():
     global files
+    global users
     global tsan
 
     tsan.login(username="robot", password="robot")
     if not tsan.token:
         print("Login Fail")
         return
+    print("Login Success")
 
     tsan.get_end_requests()
     for request in tsan.requests:
         try:
             files = tsan.download(request)
+            users = tsan.getLabelPerUser(request)
             idx = int(request['category']['idx'])
             if idx == 1: # 텍스트 객관식
                 text_select(request)
