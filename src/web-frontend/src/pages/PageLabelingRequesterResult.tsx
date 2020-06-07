99453eb206e95d1c0df56c25b69a758a04d56f51
jjolbo <jeongha3751@kookmin.ac.kr>
2020-06-08 02:00:21 +0900

    refactor: Hide the result before rewarding

--- src/web-frontend/src/pages/PageLabelingRequesterResult.tsx
+++ src/web-frontend/src/pages/PageLabelingRequesterResult.tsx
@@ -107,55 +107,55 @@ class PageLabelingRequesterResult extends React.Component<Props, RouteComponentP
             </Grid>
           </>
               ) : (
-                <Grid>
-            <Grid.Row columns={2}>
-              <Grid.Column>
-                <Header as={'h3'}># Labeling 결과 분석</Header>
-                <PieChart width={400} height={400}>
-                  <Pie dataKey="value" isAnimationActive={false} data={this.props.labelingResultStore?.labelingResult} cx={200} cy={200} outerRadius={160} label>
-                    {this.props.labelingResultStore?.labelingResult.map((entry: any, index: any) => <Cell key={index} fill={this.props.labelingResultStore?.chartColors[index % this.props.labelingResultStore?.chartColors.length]} />)}
-                  </Pie>
-                  <Tooltip />
-                </PieChart>
-              </Grid.Column>
-              <Grid.Column>
-                <Header as={'h3'}># 프로젝트 참여자 신뢰도 분석</Header>
-                <div className={'result_line'}>
-                  <LineChart
-                    width={500}
-                    height={300}
-                    data={this.props.labelingResultStore?.levelData}
-                  >
-                    <CartesianGrid strokeDasharray="3 3" />
-                    <XAxis dataKey="name" />
-                    <YAxis />
-                    <Tooltip />
-                    <Legend />
-                    <Line type="monotone" dataKey="All_user" stroke="#1D3B98" activeDot={{ r: 8 }} />
-                    <Line type="monotone" dataKey="Project_user" stroke="#98301D" />
-                  </LineChart>
-                </div>
-              </Grid.Column>
-            </Grid.Row>
-            <Grid.Row columns={2}>
-              <Grid.Column>
-                <Header as={'h4'}>의뢰하신 레이블링된 결과는 다음과 같습니다.</Header>
-                <ProjectListTable header={this.header} body={this.props.labelingResultStore?.labelingResult.map((item: any, idx: number) => {
-                  return (
-                    <Table.Row key={idx}>
-                      <Table.Cell>{item.name}</Table.Cell>
-                      <Table.Cell>{item.value}</Table.Cell>
-                    </Table.Row>
-                  );
-                })} />
-              </Grid.Column>
-              <Grid.Column className={'result_level'}>
-                <em style={{ color: "#1D3B98" }}><b>푸른 선</b></em>은 T-SAN의 <b>모든 레이블링 참여자</b>의 신뢰도 분포도입니다.<br />
-                <em style={{ color: "#98301D" }}><b>붉은 선</b></em>은 <b>이 프로젝트에 참여한 레이블링 참여자</b>의 신뢰도 분포도입니다. <br />
-                <Header as={'h3'}>이 프로젝트는 에베레스트 등급의 참여자가 많이 참여하였습니다.</Header>
-              </Grid.Column>
-            </Grid.Row>
-          </Grid>
+                  this.props.labelingPageStore?.request.state === 'REW' && <Grid>
+                      <Grid.Row columns={2}>
+                        <Grid.Column>
+                          <Header as={'h3'}># Labeling 결과 분석</Header>
+                          <PieChart width={400} height={400}>
+                            <Pie dataKey="value" isAnimationActive={false} data={this.props.labelingResultStore?.labelingResult} cx={200} cy={200} outerRadius={160} label>
+                              {this.props.labelingResultStore?.labelingResult.map((entry: any, index: any) => <Cell key={index} fill={this.props.labelingResultStore?.chartColors[index % this.props.labelingResultStore?.chartColors.length]} />)}
+                            </Pie>
+                            <Tooltip />
+                          </PieChart>
+                        </Grid.Column>
+                        <Grid.Column>
+                          <Header as={'h3'}># 프로젝트 참여자 신뢰도 분석</Header>
+                          <div className={'result_line'}>
+                            <LineChart
+                                width={500}
+                                height={300}
+                                data={this.props.labelingResultStore?.levelData}
+                            >
+                              <CartesianGrid strokeDasharray="3 3" />
+                              <XAxis dataKey="name" />
+                              <YAxis />
+                              <Tooltip />
+                              <Legend />
+                              <Line type="monotone" dataKey="All_user" stroke="#1D3B98" activeDot={{ r: 8 }} />
+                              <Line type="monotone" dataKey="Project_user" stroke="#98301D" />
+                            </LineChart>
+                          </div>
+                        </Grid.Column>
+                      </Grid.Row>
+                      <Grid.Row columns={2}>
+                        <Grid.Column>
+                          <Header as={'h4'}>의뢰하신 레이블링된 결과는 다음과 같습니다.</Header>
+                          <ProjectListTable header={this.header} body={this.props.labelingResultStore?.labelingResult.map((item: any, idx: number) => {
+                            return (
+                                <Table.Row key={idx}>
+                                  <Table.Cell>{item.name}</Table.Cell>
+                                  <Table.Cell>{item.value}</Table.Cell>
+                                </Table.Row>
+                            );
+                          })} />
+                        </Grid.Column>
+                        <Grid.Column className={'result_level'}>
+                          <em style={{ color: "#1D3B98" }}><b>푸른 선</b></em>은 T-SAN의 <b>모든 레이블링 참여자</b>의 신뢰도 분포도입니다.<br />
+                          <em style={{ color: "#98301D" }}><b>붉은 선</b></em>은 <b>이 프로젝트에 참여한 레이블링 참여자</b>의 신뢰도 분포도입니다. <br />
+                          <Header as={'h3'}>이 프로젝트는 에베레스트 등급의 참여자가 많이 참여하였습니다.</Header>
+                        </Grid.Column>
+                      </Grid.Row>
+                    </Grid>
               )
             )
           }
@@ -170,7 +170,7 @@ class PageLabelingRequesterResult extends React.Component<Props, RouteComponentP
                   <Button color='yellow' onClick={() => this.handleConfirm('verify')}>검증하기</Button> :
                     this.props.labelingPageStore?.request.state === 'VER'?
                   <>검증 중입니다.</> :
-                    this.props.labelingPageStore?.request.state !== 'VED' ?
+                    this.props.labelingPageStore?.request.state === 'VED' ?
                   <Button color='green' onClick={() => this.handleConfirm('reward')}>보상하기</Button>:
                   <>보상이 완료되었습니다.</>}
             </div>
